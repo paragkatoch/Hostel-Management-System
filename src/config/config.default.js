79c6bc773c63b6d4527a02440307b6703b21138b
Parag Katoch <paragkatoch5@gmail.com>
2021-08-02 17:10:49 +0530

    added configuration files

---
+++ src/config/config.default.js
@@ -0,0 +1,35 @@
+const constants = require("../core/constants");
+
+/**
+ * Default configuration
+ */
+const defaultConfig = {
+  app: {
+    name: "hms",
+    title: "Hostel Management System",
+  },
+  cors: {
+    enabled: true,
+    options: null,
+  },
+  helmet: {
+    enabled: true,
+    options: {},
+  },
+  morgan: {
+    enabled: true,
+    format: constants.MORGAN_FORMAT_DEV,
+  },
+  mongo: {
+    uri: "This will be overriden by .env variable MONGO_URI",
+  },
+  seed: {
+    logging: true,
+    users: [],
+  },
+  server: {
+    port: 2606, // Can be overriden by .env variable PORT
+  },
+};
+
+module.exports = defaultConfig;
