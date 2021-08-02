cc459f5dbadf2db378146087799e416d4871b40d
Parag Katoch <paragkatoch5@gmail.com>
2021-08-02 17:14:23 +0530

    connected app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const http = require("http");
+const chalk = require("chalk");
+
+const config = require("./src/config");
+
+// models
+require("./src/core/mongoose");
+
+const app = require("./src/core/express");
+
+http.createServer(app).listen(config.server.port || 2606, (err) => {
+  if (err)
+    console.log(
+      chalk.red(
+        `\n[-] Error occured while starting sever on PORT:${config.server.port}`
+      )
+    );
+
+  console.log(
+    chalk.green(`\n[*] API server is listening on port ${config.server.port}`)
+  );
+});
