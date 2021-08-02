a0c0ad42d5fbccebe58d5a95247f2fe598391f06
Parag Katoch <paragkatoch5@gmail.com>
2021-08-02 17:12:40 +0530

    added constants

---
+++ src/core/constants.js
@@ -0,0 +1,21 @@
+const ENV_DEV = "development";
+const ENV_PROD = "production";
+
+const MORGAN_FORMAT_COMBINED = "combined";
+const MORGAN_FORMAT_DEV = "dev";
+
+const ROLE_ADMIN = "admin";
+const ROLE_ROOT = "root";
+const ROLE_USER = "user";
+
+module.exports = {
+  ENV_DEV,
+  ENV_PROD,
+
+  MORGAN_FORMAT_COMBINED,
+  MORGAN_FORMAT_DEV,
+
+  ROLE_ADMIN,
+  ROLE_ROOT,
+  ROLE_USER,
+};
