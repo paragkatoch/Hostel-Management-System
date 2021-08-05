010518e9ce2fce20ab674a5c7475056c77449241
Parag Katoch <paragkatoch5@gmail.com>
2021-08-05 20:18:36 +0530

    added token to config

--- src/config/config.default.js
+++ src/config/config.default.js
@@ -4,32 +4,43 @@ const constants = require("../core/constants");
  * Default configuration
  */
 const defaultConfig = {
-  app: {
-    name: "hms",
-    title: "Hostel Management System",
-  },
-  cors: {
-    enabled: true,
-    options: null,
-  },
-  helmet: {
-    enabled: true,
-    options: {},
-  },
-  morgan: {
-    enabled: true,
-    format: constants.MORGAN_FORMAT_DEV,
-  },
-  mongo: {
-    uri: "This will be overriden by .env variable MONGO_URI",
-  },
-  seed: {
-    logging: true,
-    users: [],
-  },
-  server: {
-    port: 2606, // Can be overriden by .env variable PORT
-  },
+	app: {
+		name: "hms",
+		title: "Hostel Management System",
+	},
+	cors: {
+		enabled: true,
+		options: null,
+	},
+	helmet: {
+		enabled: true,
+		options: {},
+	},
+	morgan: {
+		enabled: true,
+		format: constants.MORGAN_FORMAT_DEV,
+	},
+	mongo: {
+		uri: "This will be overriden by .env variable MONGO_URI",
+	},
+	seed: {
+		logging: true,
+		users: [],
+	},
+	server: {
+		port: 2606, // Can be overriden by .env variable PORT
+	},
+	token: {
+		purpose: {
+			auth: "authentication",
+			changePassword: "changepassword",
+		},
+		expire: {
+			authentication: "7d",
+			changepassword: "3h",
+		},
+		algorithm: "RS256",
+	},
 };
 
 module.exports = defaultConfig;
