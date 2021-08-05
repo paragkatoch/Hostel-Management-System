51cf3f99160d973b58de3808440e9a5af1085dcb
Parag Katoch <paragkatoch5@gmail.com>
2021-08-05 18:13:08 +0530

    added secret keys to the config

--- src/config/index.js
+++ src/config/index.js
@@ -14,32 +14,38 @@ dotenv.config({ path: path.resolve(__dirname, "../../.env") });
  * Joi schema for validating environment variables
  */
 const envVarsSchema = Joi.object({
-  MONGO_URI: Joi.string().uri().required(),
-  NODE_ENV: Joi.string()
-    .valid(constants.ENV_DEV, constants.ENV_PROD)
-    .required(),
+	MONGO_URI: Joi.string().uri().required(),
+	NODE_ENV: Joi.string()
+		.valid(constants.ENV.DEV, constants.ENV.PROD)
+		.required(),
+	PRIV_KEY: Joi.string().required(),
+	PUB_KEY: Joi.string().required(),
 }).unknown();
 
 // Validation
 const { value, error } = envVarsSchema.validate(process.env);
 if (error) {
-  console.log(
-    chalk.red(
-      '\n[-] Invalid environment variables. Please edit the ".env" file and restart the process.'
-    )
-  );
-  throw new Error(error.message);
+	console.log(
+		chalk.red(
+			'\n[-] Invalid environment variables. Please edit the ".env" file and restart the process.'
+		)
+	);
+	throw new Error(error.message);
 }
 
 const envConfig = {
-  env: value.NODE_ENV,
-  mongo: {
-    uri: value.MONGO_URI,
-  },
+	env: value.NODE_ENV,
+	mongo: {
+		uri: value.MONGO_URI,
+	},
+	key: {
+		priv: value.PRIV_KEY,
+		pub: value.PUB_KEY,
+	},
 };
 
 // Final config
-let config = envConfig.env === constants.ENV_DEV ? configDev : configProd;
+let config = envConfig.env === constants.ENV.DEV ? configDev : configProd;
 
 config = _.merge({}, config, envConfig);
 
