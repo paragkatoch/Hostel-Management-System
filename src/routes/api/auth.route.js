61a273274ab19e698da0a2353285c62f95240f6e
Parag Katoch <paragkatoch5@gmail.com>
2021-08-08 00:22:42 +0530

    implemented login route

--- src/routes/api/auth.route.js
+++ src/routes/api/auth.route.js
@@ -1,15 +1,31 @@
 const router = require("express").Router();
+const auth = require("../../controllers/auth.controller");
 
+const localAuthentication =
+	require("../../middleware/createAuthenticationMiddleware")("local");
+
+// Todo allow login if has correct auth token
 // login
-router.post("/login");
-
-// signup
-router.post("signup");
-
-// changepassword
-router.post("/changepassword/:token");
+// Todo: should check user and if everything ok then should return a jwt token
+router.post(
+	"/login",
+	auth.validateLoginPayload,
+	localAuthentication,
+	auth.login
+);
 
 // logout
+// Todo: remove jwt token of the user
 router.post("/logout");
+
+// signup
+// Todo: should check the user if exists in db or already registered and then should return an appropriate response
+router.post("/signup");
+
+// changepassword
+// Todo: verify the token
+router.get("/changepassword/:token");
+// Todo: verify and change password
+router.post("/changepassword/:token");
 
 module.exports = router;
