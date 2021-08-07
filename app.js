e396289d4ad9f14acd934311ca99988ec818e7cc
Parag Katoch <paragkatoch5@gmail.com>
2021-08-08 00:13:21 +0530

    [fix] User model not found

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const http = require("http");
 const chalk = require("chalk");
 
-const config = require("./src/config");
-const app = require("./src/core/express");
-
 // models
 require("./src/core/mongoose");
+const config = require("./src/config");
+const app = require("./src/core/express");
 
 http.createServer(app).listen(config.server.port || 2606, (err) => {
 	if (err)
 		console.log(
 			chalk.red(
-				`\n[-] Error occured while starting sever on PORT:${config.server.port}`
+				`\n[-] Error occurred while starting sever on PORT:${config.server.port}`
 			)
 		);
 
